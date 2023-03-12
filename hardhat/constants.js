2262818c408c6c85aa25eca898b9f249305b6baf
getzelus <chamoloss@live.fr>
2023-03-12 23:54:47 +0100

    begin

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = " 0x7D8E21F8a687d895F74a3662454D899B4C8eAF55";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
