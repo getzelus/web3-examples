2170a13de9cd260356c9411f9659717d1c735782
getzelus <chamoloss@live.fr>
2023-03-13 00:51:02 +0100

    new nft contract with metadata

--- hardhat/constants.js
+++ hardhat/constants.js
@@ -1,6 +1,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x7D8E21F8a687d895F74a3662454D899B4C8eAF55";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://web3-examples-xi.vercel.app/api/";
 
 // cryptogetz 
 // 0x79D929709FcDe55Acc280A0303979F5a95322214
